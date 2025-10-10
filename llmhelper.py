60a890409313ccd5ed4c74478a61d595e4e1f1f6
Musfikur Rahaman <shuvo.musfikur@gmail.com>
2025-10-10 16:14:25 -0500

    Update llmhelper.py

--- llmhelper.py
+++ llmhelper.py
@@ -12,16 +12,17 @@ API_KEY = os.getenv("GROQ_API_KEY") or st.secrets.get("GROQ_API_KEY")
 
 llm = ChatGroq(api_key=API_KEY, model_name="meta-llama/llama-4-scout-17b-16e-instruct")
 
+
+
+
 def explain_fake_news(text):
-
-
-
     prompt = (
         f"The following news article has been classified as FAKE.\n"
         f"Explain in a few sentences why this might be fake news:\n\n{text}\n\nExplanation:"
     )
     response = llm.invoke(prompt)
     return response.content.strip()
+
 
 if __name__ == "__main__":
     sample = "AI is taking over the world next year."
